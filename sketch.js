572fbf58fe28659772945b50176aa51914ffd310
Tyson-07 <72865445+Tyson-07@users.noreply.github.com>
2020-11-05 16:47:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2;
+var g;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,250,50,50);
+    box2 = new Box(250,200,100,50);
+    g = new Ground(200,390,400,15);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    g.display();
+   
+}
